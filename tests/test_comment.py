af0e87482715e9a58bc27e10e2b7bc9fd9b963fc
Balazs Eszes <c4deszes@gmail.com>
2020-11-15 16:36:14 +0100

    Fixed typo in comment capturing tests

--- tests/test_comment.py
+++ tests/test_comment.py
@@ -19,11 +19,11 @@ def test_comment_collection_lin21():
 	path = os.path.join(os.path.dirname(__file__), "ldf", "lin21.ldf")
 	ldf = ldfparser.parseLDF(path, captureComments=True)
 	assert len(ldf.comments) >= 0
-	assert "// Source: https://lin-cia.org/fileadmin/microsites/lin-cia.org/resources/documents/LIN-Spec_Pac2_1.pdf' in ldf.comments" in ldf.comments
+	assert "// Source: https://lin-cia.org/fileadmin/microsites/lin-cia.org/resources/documents/LIN-Spec_Pac2_1.pdf" in ldf.comments
 
 
 def test_comment_collection_lin22():
 	path = os.path.join(os.path.dirname(__file__), "ldf", "lin22.ldf")
 	ldf = ldfparser.parseLDF(path, captureComments=True)
 	assert len(ldf.comments) >= 0
-	assert "// Source: https://lin-cia.org/fileadmin/microsites/lin-cia.org/resources/documents/LIN_2.2A.pdf' in ldf.comments" in ldf.comments
+	assert "// Source: https://lin-cia.org/fileadmin/microsites/lin-cia.org/resources/documents/LIN_2.2A.pdf" in ldf.comments

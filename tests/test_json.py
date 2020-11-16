f27ed358212c0debb465b097785e70ea65135366
Balazs Eszes <c4deszes@gmail.com>
2020-11-16 21:04:23 +0100

    Snapshot testing
    Added script that generates snapshot data
    Added workflow that executes snapshot tests against target branch
    Added new marker to pytest 'snapshot'
    Added test for comparing JSON output of the LDF parser to snapshot

---
+++ tests/test_json.py
@@ -0,0 +1,23 @@
+import ldfparser
+import os
+import json
+import glob
+import difflib
+import pytest
+
+ldf_directory = os.path.join(os.path.dirname(__file__), 'ldf')
+snapshot_directory = os.path.join(os.path.dirname(__file__), 'snapshot')
+ldf_files = glob.glob(ldf_directory + '/*.ldf')
+
+@pytest.mark.snapshot
+@pytest.mark.parametrize(
+	('ldf_path'),
+	ldf_files
+)
+def test_compare_json(ldf_path):
+	snapshot_file = os.path.join(snapshot_directory, os.path.basename(ldf_path)) + '.json'
+	with open(snapshot_file, 'r') as snapshot:
+		snapshot_content = snapshot.read()
+		current = json.dumps(ldfparser.parseLDFtoDict(ldf_path))
+		diff = ''.join(difflib.unified_diff(snapshot_content, current))
+		assert not bool(diff), f"{ldf_path} not matching snapshot: {diff}"

d9c2fa9add8335fac15cd249ca98be427e3bae3e
Balázs Eszes <c4deszes@gmail.com>
2020-11-16 21:25:47 +0100

    Snapshot testing (#25)
    * Snapshot testing
    
    Added script that generates snapshot data
    Added workflow that executes snapshot tests against target branch
    Added new marker to pytest 'snapshot'
    Added test for comparing JSON output of the LDF parser to snapshot

--- tests/test_comment.py
+++ tests/test_comment.py
@@ -1,7 +1,9 @@
+import pytest
 import os
 import ldfparser
 
 
+@pytest.mark.integration
 def test_comment_collection_lin13():
 	path = os.path.join(os.path.dirname(__file__), "ldf", "lin13.ldf")
 	ldf = ldfparser.parseLDF(path, captureComments=True)
@@ -9,12 +11,14 @@ def test_comment_collection_lin13():
 	assert '// This is a LIN description example file' in ldf.comments
 
 
+@pytest.mark.integration
 def test_comment_collection_lin20():
 	path = os.path.join(os.path.dirname(__file__), "ldf", "lin20.ldf")
 	ldf = ldfparser.parseLDF(path, captureComments=True)
 	assert len(ldf.comments) >= 0
 
 
+@pytest.mark.integration
 def test_comment_collection_lin21():
 	path = os.path.join(os.path.dirname(__file__), "ldf", "lin21.ldf")
 	ldf = ldfparser.parseLDF(path, captureComments=True)
@@ -22,6 +26,7 @@ def test_comment_collection_lin21():
 	assert "// Source: https://lin-cia.org/fileadmin/microsites/lin-cia.org/resources/documents/LIN-Spec_Pac2_1.pdf" in ldf.comments
 
 
+@pytest.mark.integration
 def test_comment_collection_lin22():
 	path = os.path.join(os.path.dirname(__file__), "ldf", "lin22.ldf")
 	ldf = ldfparser.parseLDF(path, captureComments=True)

30622b246cd271294c381e3b85423df7ae2f694c
Ayoub Kaanich <ayoub.kaanich@technica-engineering.de>
2020-11-25 23:17:45 +0100

    Allow signal with no subscribers and empty node attribute section (#28)

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -71,3 +71,15 @@ def test_load_valid_lin22():
 	converter = ldf.converters['InternalLightsRequest']
 	assert converter.name == 'Dig2Bit'
 	assert isinstance(converter._converters[0], LogicalValue)
+
+@pytest.mark.unit
+def test_no_signal_subscribers():
+	path = os.path.join(os.path.dirname(__file__), "ldf", "no_signal_subscribers.ldf")
+	ldf = ldfparser.parseLDF(path)
+
+	assert ldf.protocol_version == 2.2
+	assert ldf.language_version == 2.2
+	assert ldf.baudrate == 19200
+	
+	assert ldf.signal('DummySignal_0') is not None
+	assert ldf.frame('DummyFrame') is not None

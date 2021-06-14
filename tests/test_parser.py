10cc70d918b37a64e845c68ab1f3c9b2788215f5
Jules Laffitte <jules.laffitte.mobil@gmail.com>
2021-06-14 14:50:25 -0600

    Fixing Flake8 errors

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -84,6 +84,7 @@ def test_load_valid_lin22():
 	assert converter.name == 'Dig2Bit'
 	assert isinstance(converter._converters[0], LogicalValue)
 
+
 @pytest.mark.unit
 def test_no_signal_subscribers():
 	path = os.path.join(os.path.dirname(__file__), "ldf", "no_signal_subscribers.ldf")
@@ -95,6 +96,7 @@ def test_no_signal_subscribers():
 
 	assert ldf.signal('DummySignal_0') is not None
 	assert ldf.frame('DummyFrame') is not None
+
 
 @pytest.mark.unit
 def test_load_valid_lin_encoders():

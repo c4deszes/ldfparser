a0a242768e50132f8fe2854f604e03f040805fcc
Balazs Eszes <c4deszes@gmail.com>
2021-10-02 11:07:56 +0200

    Increases coverage of LinVersion tests

--- tests/test_lin.py
+++ tests/test_lin.py
@@ -2,42 +2,113 @@ import pytest
 from ldfparser.lin import LIN_VERSION_1_3, LIN_VERSION_2_0, LIN_VERSION_2_1, LIN_VERSION_2_2
 
 @pytest.mark.unit()
-def test_linversion_str():
-    assert str(LIN_VERSION_2_0) == "2.0"
+@pytest.mark.parametrize(
+    ('version', 'expected'),
+    [
+        (LIN_VERSION_1_3, "1.3"),
+        (LIN_VERSION_2_0, "2.0"),
+        (LIN_VERSION_2_1, "2.1"),
+        (LIN_VERSION_2_2, "2.2")
+    ]
+)
+def test_linversion_str(version, expected):
+    assert str(version) == expected
 
 @pytest.mark.unit()
-def test_linversion_equal_version():
-    assert LIN_VERSION_2_0 == LIN_VERSION_2_0
+@pytest.mark.parametrize(
+    ('a', 'b'),
+    [
+        (LIN_VERSION_1_3, LIN_VERSION_1_3),
+        (LIN_VERSION_2_0, LIN_VERSION_2_0),
+        (LIN_VERSION_2_1, LIN_VERSION_2_1)
+    ]
+)
+def test_linversion_equal_version(a, b):
+    assert a == b
 
 @pytest.mark.unit()
-def test_linversion_not_equal_version():
-    assert LIN_VERSION_1_3 != LIN_VERSION_2_1
-    assert LIN_VERSION_2_0 != LIN_VERSION_2_1
-    assert LIN_VERSION_2_1 != LIN_VERSION_2_2
+@pytest.mark.parametrize(
+    ('a', 'b'),
+    [
+        (LIN_VERSION_1_3, LIN_VERSION_2_1),
+        (LIN_VERSION_2_0, LIN_VERSION_2_1),
+        (LIN_VERSION_2_2, LIN_VERSION_2_1)
+    ]
+)
+def test_linversion_not_equal_version(a, b):
+    assert a != b
 
 @pytest.mark.unit()
-def test_linversion_less_than_version():
-    assert LIN_VERSION_1_3 < LIN_VERSION_2_1
-    assert LIN_VERSION_2_0 < LIN_VERSION_2_1
-    assert LIN_VERSION_2_1 < LIN_VERSION_2_2
+@pytest.mark.parametrize(
+    ('a', 'b'),
+    [
+        (LIN_VERSION_1_3, LIN_VERSION_2_1),
+        (LIN_VERSION_2_0, LIN_VERSION_2_1),
+        (LIN_VERSION_2_1, LIN_VERSION_2_2)
+    ]
+)
+def test_linversion_less_than_version(a, b):
+    assert a < b
 
 @pytest.mark.unit()
-def test_linversion_greater_than_version():
-    assert not LIN_VERSION_1_3 > LIN_VERSION_2_0
-    assert LIN_VERSION_2_0 > LIN_VERSION_1_3
-    assert LIN_VERSION_2_1 > LIN_VERSION_2_0
-    assert LIN_VERSION_2_2 > LIN_VERSION_2_1
+@pytest.mark.parametrize(
+    ('a', 'b'),
+    [
+        (LIN_VERSION_2_0, LIN_VERSION_1_3),
+        (LIN_VERSION_2_1, LIN_VERSION_2_0),
+        (LIN_VERSION_2_2, LIN_VERSION_2_1)
+    ]
+)
+def test_linversion_greater_than_version(a, b):
+    assert a > b
 
 @pytest.mark.unit()
-def test_linversion_less_than_equal_version():
-    assert LIN_VERSION_2_0 <= LIN_VERSION_2_1
-    assert LIN_VERSION_2_1 <= LIN_VERSION_2_1
+@pytest.mark.parametrize(
+    ('a', 'b'),
+    [
+        (LIN_VERSION_1_3, LIN_VERSION_1_3),
+        (LIN_VERSION_1_3, LIN_VERSION_2_0),
+        (LIN_VERSION_1_3, LIN_VERSION_2_1)
+    ]
+)
+def test_linversion_less_than_equal_version(a, b):
+    assert a <= b
 
 @pytest.mark.unit()
-def test_linversion_greater_than_equal_version():
-    assert LIN_VERSION_2_1 >= LIN_VERSION_2_1
-    assert LIN_VERSION_2_1 >= LIN_VERSION_2_0
+@pytest.mark.parametrize(
+    ('a', 'b'),
+    [
+        (LIN_VERSION_2_0, LIN_VERSION_1_3),
+        (LIN_VERSION_2_0, LIN_VERSION_2_0),
+        (LIN_VERSION_2_1, LIN_VERSION_2_0)
+    ]
+)
+def test_linversion_greater_than_equal_version(a, b):
+    assert a >= b
 
 @pytest.mark.unit()
-def test_linversion_float():
-    assert float(LIN_VERSION_2_2) == 2.2
+@pytest.mark.parametrize(
+    ('version', 'expected'),
+    [
+        (LIN_VERSION_1_3, 1.3),
+        (LIN_VERSION_2_0, 2.0),
+        (LIN_VERSION_2_1, 2.1),
+        (LIN_VERSION_2_2, 2.2)
+    ]
+)
+def test_linversion_float(version, expected):
+    assert float(version) == expected
+
+@pytest.mark.unit()
+@pytest.mark.parametrize(
+    ('func', 'arg'),
+    [
+        (LIN_VERSION_1_3.__gt__, "2.0"),
+        (LIN_VERSION_1_3.__lt__, "2.0"),
+        (LIN_VERSION_1_3.__ge__, "2.0"),
+        (LIN_VERSION_1_3.__le__, "2.0")
+    ]
+)
+def test_linversion_typerror(func, arg):
+    with pytest.raises(TypeError):
+        func(arg)

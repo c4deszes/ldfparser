ec7418cc85a39a4e147988fe2d2a04f89561f966
Balazs Eszes <c4deszes@gmail.com>
2021-10-02 08:48:14 +0200

    Fixes read comment example

--- examples/read_comments.py
+++ examples/read_comments.py
@@ -3,5 +3,5 @@ import ldfparser
 
 if __name__ == "__main__":
     path = os.path.join(os.path.dirname(__file__), 'lin22.ldf')
-    ldf = ldfparser.parse_ldf(path, captureComments=True)
+    ldf = ldfparser.parse_ldf(path, capture_comments=True)
     print(ldf.comments)
